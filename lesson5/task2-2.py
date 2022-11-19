da58ac488bc22ecbf20b16b8226f13c933db796e
Valeriy Lazarenko <lvv0529@gmail.com>
2022-11-19 15:23:06 +0200

    task2-2 works, but task2 - still not

---
+++ lesson5/task2-2.py
@@ -0,0 +1,38 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+cmap_bold = ListedColormap(['blue', '#FFFF00', 'black', 'green'])
+X, y = make_blobs(n_samples=300, n_features=2, centers=8,
+                  cluster_std=1.3, random_state=4)
+y = y % 2
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    random_state=4)
+k_range = range(1, 26)
+scores = {}
+scores_list = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_predict = knn.predict(X_test)
+    scores[k] = metrics.accuracy_score(y_test, y_predict)
+    scores_list.append(metrics.accuracy_score(y_test, y_predict))
+
+# Show results
+the_best_neighbours = max(scores, key=scores.get)
+print("Scores: ")
+for i in scores:
+    print(i, ' - ', scores[i])
+print("The best score with {} neighbors. Score = {}".format(the_best_neighbours,
+                                                            scores[the_best_neighbours]))
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Horizontally stacked subplots')
+ax1.scatter(X[:, 0], X[:, 1], c=y,
+            marker='o', s=30, cmap=cmap_bold)
+# ax2.plot(x, -y)
+plt.show()
